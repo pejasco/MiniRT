62d165509662799b54e3e301dfa3fc7346c4024a
pejasco <pejasco@gmail.com>
2024-07-25 23:50:52 +0100

    haha

--- src/matrix2.c
+++ src/matrix2.c
@@ -6,7 +6,7 @@
 /*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 23:06:19 by chuleung          #+#    #+#             */
-/*   Updated: 2024/07/25 20:49:17 by chuleung         ###   ########.fr       */
+/*   Updated: 2024/07/25 23:50:39 by chuleung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ void print_matrix(t_matrix_4x4 *mtx)
     printf("#########################################################\n");
 }
 
+
 t_matrix_4x4	create_matrix_filled_with_zero(void)
 {
 	t_matrix_4x4	mtx_with_zero;
@@ -57,7 +58,7 @@ t_matrix_4x4	create_matrix_filled_with_zero(void)
 	return (mtx_with_zero);
 }
 
-double	determinant_matrix_(t_matrix_4x4 *mtx)
+double	calculate_determinant_matrix_2x2(t_matrix_4x4 *mtx)
 {
 	double	result;
 
@@ -96,10 +97,90 @@ t_matrix_4x4	find_submatrix(t_matrix_4x4 *mtx, int omit_row,
 	return (smtx_vars.smtx);
 }
 
+double	calculate_minor_matrix_3x3(t_matrix_4x4 *mtx, int omit_row,
+					int omit_col, int smtx_dimen)
+{
+	t_matrix_4x4	submatrix;
+	double			minor;
+
+	submatrix = find_submatrix(mtx, omit_row, omit_col, smtx_dimen);
+	minor = calculate_determinant_matrix_2x2(&submatrix);
+	return (minor);
+}
+
+double calculate_cofactor_matrix_3x3(t_matrix_4x4 *mtx, int omit_row,
+					int omit_col, int smtx_dimen)
+{
+	double			minor;
+	double			cofactor;
+
+	minor = calculate_minor_matrix_3x3(mtx, omit_row,
+			omit_col, smtx_dimen);
+	if ((omit_row + omit_row) % 2 == 0)
+		cofactor = minor;
+	else
+		cofactor = -minor;
+	return (cofactor);
+}
+
+
+double	calculate_determinant_matrix_3x3(t_matrix_4x4 *mtx)
+{
+	double	result;
+
+	result = (mtx->entries[0][0] * mtx->entries[1][1]
+			- mtx->entries[0][1] * mtx->entries[1][0]);
+	return (result);
+}
+
+//for simplicity using the 1st row
+double	calculate_determinant_matrix_4x4(t_matrix_4x4 *mtx)
+{
+	double	cofactor_row_1_x_col_1;
+	double	cofactor_row_1_x_col_2;
+	double	cofactor_row_1_x_col_3;
+
+
+
+}
+
+
+
+
 int	main(void)
 {
 	t_matrix_4x4	test;
-	t_matrix_4x4	result;
+	double			minor_1;
+	double			cofactor_1;
+	double			minor_2;
+	double			cofactor_2;
+
+	test = (t_matrix_4x4){
+		.entries = {
+	{3, 5, 0, 0},
+	{2, -1, -7, 0},
+	{6, -1, 5, 0},
+	{0, 0, 0, 0},
+	}};
+	minor_1 = calculate_minor_matrix_3x3(&test, 0, 0, 2);
+	cofactor_1 = calculate_cofactors_matrix_3x3(&test, 0, 0, 2);
+	minor_2 = calculate_minor_matrix_3x3(&test, 1, 0, 2);
+	cofactor_2 = calculate_cofactor_matrix_3x3(&test, 1, 0, 2);
+	printf("minor: %f; cofactor: %f;\n", minor_1, cofactor_1);
+	printf("minor: %f; cofactor: %f;\n", minor_2, cofactor_2);
+}
+
+
+
+/*
+int	main(void)
+{
+	t_matrix_4x4	test_1;
+	double			result_1;
+	t_matrix_4x4	test_2;
+	t_matrix_4x4	result_2;
+	double	res;
+
 
 	test = (t_matrix_4x4){
 		.entries = {
@@ -110,9 +191,31 @@ int	main(void)
 	}};
 	result = find_submatrix(&test, 2, 1, 3);
 	print_matrix(&result);
+
+	test_1 = (t_matrix_4x4){
+		.entries = {
+	{3, 5, 0, 0},
+	{2, -1, -7, 0},
+	{6, -1, 5, 0},
+	{0, 0, 0, 0},
+	}};
+	result_1 = calculate_minor_matrix_3x3(&test_1);
+	printf("result: %f\n", result_1);
+
+	test_2 = (t_matrix_4x4){
+		.entries = {
+	{3, 5, 0, 0},
+	{2, -1, -7, 0},
+	{6, -1, 5, 0},
+	{0, 0, 0, 0},
+	}};
+	result_2 = find_submatrix(&test_2, 1, 0, 2);
+	print_matrix(&result_2);
+	res = calculate_determinant_matrix_2x2(&result_2);
+	printf("result: %f\n", res);
 	return (0);
 }
-
+*/
 
 
 
